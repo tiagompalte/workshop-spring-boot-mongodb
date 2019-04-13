27b32fbc8a86ad658664e83f25786cc59332c3cb
Tiago Mariani Palte <tiagompalte@hotmail.com>
2019-04-13 09:33:30 -0300

    First Commit

---
+++ src/main/java/com/tiagompalte/wsbm/WorkshopSpringBootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.tiagompalte.wsbm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringBootMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopSpringBootMongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tiagompalte/wsbm/WorkshopSpringBootMongodbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tiagompalte.wsbm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorkshopSpringBootMongodbApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
